a686455f06e79751d4cc162c7350952498a4eec5
Anastasis Germanidis <agermanidis@gmail.com>
2019-12-01 20:37:59 -0500

    add threshold parameter

--- runway_model.py
+++ runway_model.py
@@ -12,22 +12,22 @@ def setup(opts):
   return load_learner('.', opts['checkpoint'])
 
 
-@runway.command('mask', inputs={'image': runway.image}, outputs={'segmented': runway.segmentation(label_to_id={'background': 0, 'person': 1}, label_to_color={'background': [0,0,0], 'person': [255, 255, 255]})})
+@runway.command('mask', inputs={'image': runway.image, 'threshold': runway.number(default=0.9, min=0, max=1, step=0.001)}, outputs={'image': runway.image})
 def mask(learner, inputs):
   inp = inputs['image'].convert('RGB')
   original_size = inp.size
   inp_resized = inp.resize((512, 512))
   im = Image(pil2tensor(inp_resized, np.float32).div(255))
   result = learner.predict(im)
-  np_img = image2np(result[1].data)
+  np_img = result[2].data.numpy()[1]
+  np_img[np_img > inputs['threshold']] = 1
   mask = np_img.astype(np.uint8)
-  # mask = (np_img > 0).reshape((512, 512, 1))
-  # mask = np.stack((np_img,)*3, axis=-1)
-  # mask = resize(mask, (original_size[1], original_size[0]), anti_aliasing=False).astype(np.uint8)
-  # masked = np.array(inp)
-  # masked[mask == 0] = 0
-
-  return PIL.Image.fromarray(mask).resize(original_size)
+  mask = (np_img > 0).reshape((512, 512, 1))
+  mask = np.stack((np_img,)*3, axis=-1)
+  mask = resize(mask, (original_size[1], original_size[0]), anti_aliasing=False).astype(np.uint8)
+  masked = np.array(inp)
+  masked[mask == 0] = 0
+  return masked
 
 
 if __name__ == '__main__':
